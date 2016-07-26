ecf337711518968b858faa0221c725bd1cc1304b
Cyle Carlson <jacerider@gmail.com>
2016-07-26 09:25:46 -0500

    Icon replacement utility.

--- src/Iconify.php
+++ src/Iconify.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\iconify;
 
+use Drupal\Core\Render\Markup;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
 /**
  * Defines an object that holds information about an icon.
  */
@@ -15,11 +18,25 @@ class Iconify {
   protected $text;
 
   /**
+   * The iconified icon.
+   *
+   * @var string
+   */
+  protected $icon = '';
+
+  /**
    * The Icon options.
    *
    * @var array
    */
-  protected $options = array();
+  protected $options = [
+    'iconOnly' => FALSE
+  ];
+
+  /**
+   * The system defined icon replacement definition.
+   */
+  protected $info = [];
 
   /**
    * Constructs a new Iconify object.
@@ -29,10 +46,10 @@ class Iconify {
    * @param array $options
    */
   public function __construct($text, $options = array()) {
-    $this->text = $text;
-    $this->options = $options;
+    $this->setText($text);
+    $this->setOptions($options);
+    $this->info = \Drupal::service('plugin.manager.iconify.info')->getDefinitions();
   }
-
 
   /**
    * Creates a new Iconify object for text.
@@ -45,6 +62,19 @@ class Iconify {
     return new static($text, $options);
   }
 
+  /**
+   * Set the text
+   *
+   * @param string $text
+   *  The string to find an icon for.
+   */
+  public function setText($text) {
+    if (is_a($text, '\Drupal\Core\StringTranslation\TranslatableMarkup')) {
+      $text = $text->getUntranslatedString();
+    }
+    $this->text = $text;
+    return $this;
+  }
 
   /**
    * Returns the text.
@@ -53,6 +83,68 @@ class Iconify {
    */
   public function getText() {
     return $this->text;
+  }
+
+  /**
+   * Returns the translated text.
+   */
+  public function getTextTranslated() {
+    return new TranslatableMarkup($this->getText());
+  }
+
+
+  /**
+   * Return cleaned and lowercase text.
+   */
+  public function getMachine() {
+    return strtolower($this->getText());
+  }
+
+
+  /**
+   * Finds the icon and returns its info array if it can be found.
+   */
+  public function getIconInfo() {
+    $text = $this->getMachine();
+    foreach ($this->info as $info) {
+      if ($info['text'] && $info['text'] == $text) {
+        return $info;
+      }
+      if ($info['regex'] && !$this->icon && preg_match('!' . $info['regex'] . '!', $text)) {
+        return $info;
+      }
+    }
+    return [];
+  }
+
+
+  /**
+   * Returns the icon string.
+   *
+   * @return string
+   */
+  public function getIcon() {
+    if (!$this->icon && $info = $this->getIconInfo()) {
+      return $info['icon'];
+    }
+    return '';
+  }
+
+  public function getIconMarkup() {
+    if ($icon = $this->getIcon()) {
+      return '<i class="' . $icon . '"></i>';
+    }
+    return '';
+  }
+
+  /**
+   * Set the icon.
+   *
+   * @return string
+   */
+  public function setIcon($icon) {
+    $this->icon = $icon;
+    return $this;
   }
 
   /**
@@ -89,7 +181,7 @@ class Iconify {
    * @return $this
    */
   public function setOptions($options) {
-    $this->options = $options;
+    $this->options = $options + $this->options;
     return $this;
   }
 
@@ -122,7 +214,17 @@ class Iconify {
   }
 
   public function toString() {
-    return !empty($this->options['iconOnly']) ? '[icon]' : '[icon] ' . $this->getText();
+    $icon = $this->getIconMarkup();
+    $text = $this->getTextTranslated();
+    if ($icon) {
+      $output = $icon;
+      if ($this->options['iconOnly']) {
+        $output .= ' ' . $text;
+      }
+    } else {
+      $output = $text;
+    }
+    return Markup::create($output);
   }
 
 }
